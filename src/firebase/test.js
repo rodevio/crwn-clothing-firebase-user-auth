033d61863a43e0df253be1c317f157833aeb60fd
Rodrigo Castro <rodrigocastro@iMac.lan>
2020-06-17 14:39:02 +0200

    91. . Transforming our User Auth Object, figure out the properties (for example: displayName, email, phoneNumber, uid) we need and put it in our Database. Creating a Snapshot. If a user does not exists, insert it in the database.

--- src/firebase/test.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const firestore = firebase.firestore();
-
-firestore.collection('users').doc('dasdasdas').collection('cardItems').doc('sadjasjdsa');
-firestore.doc('/users/dasdasdas/cardItems/sadjasjdsa');
-firestore.collection('/users/dasdasdas/cardItems');
